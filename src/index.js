3352aa9dd97c49d5897056e238170f5d803ca78f
grnkvch <43145804+grnkvch@users.noreply.github.com>
2018-09-15 18:53:50 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var s = number;
+  var res = 0;
+  while (s>1) {
+    s = Math.floor(s/5);
+    res+=s;
+  }
+  return res;
 }
